4600d961984e086d8aa12fd691ce0de07758465d
Suyash Sanjeev <ssuyash@uw.edu>
2022-07-07 10:35:52 -0700

    added hyperlink header styling

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,19 +7,41 @@ import {
 } from "react-router-dom";
 import Evaluator from './Evaluator';
 import Home from './Home';
+import styled from '@emotion/styled';
 
+const NavUnlisted = styled.ul`
+
+  display: flex;
+
+  a {
+    text-decoration: none;
+  }
+
+  li {
+    color: blue;
+    margin: 0 0.8rem;
+    font-size: 1.3rem;
+    position: relative;
+    list-style: none;
+  }
+
+  .current {
+    li {
+      border-bottom: 2px solid black;
+    }
+  }
+`;
 function App() {
   return (
     <Router>
-      <ul className='navBar'>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/evaluator">Evaluator</NavLink>
-        </li>
-      </ul>
-
+      <NavUnlisted>
+          <NavLink to="/">
+            <li>Home</li>
+          </NavLink>
+          <NavLink to="/evaluator">
+            <li>Evaluator</li>
+          </NavLink>
+      </NavUnlisted>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/evaluator" element={<div id="main"><Evaluator /></div>} />
