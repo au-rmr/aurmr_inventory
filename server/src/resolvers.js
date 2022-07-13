ea3ef54c5c8eae35245bbbaa7e36abedb2968dca
a380ask <a380ask@outlook.com>
2022-07-13 09:39:42 -0700

    made some more changes to graphql schema

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -13,6 +13,7 @@ module.exports = {
                 }
             })
         },
+
         getProduct: async (_, args, context, info) => {
             console.log(args)
             const where = args.filter ? {
@@ -32,6 +33,7 @@ module.exports = {
             })
             return prods;
         },
+
         getAllAttributes: (_, args, context) => {
             console.log(context.prisma.attribute.findMany())
             return context.prisma.attribute.findMany({
@@ -44,6 +46,29 @@ module.exports = {
                 }
             })
         },
+
+        filterProdByAttr: (_, args, context) => {
+            console.log(args.attributes)
+            const where = args.attributes ? {
+                OR: args.attributes.map(attrId => [{
+                    attributes: {
+                        attribute: { equals: parseInt(attrId) }
+                    }
+                }])
+            }
+                : {}
+            const prods = context.prisma.amazonProduct.findMany({
+                where,
+                include: {
+                    attributes: {
+                        include: {
+                            attribute: true
+                        }
+                    }
+                }
+            })
+            return prods
+        }
     },
 
     Mutation: {
@@ -71,6 +96,7 @@ module.exports = {
             })
             return newProduct
         },
+
         addAttribute: (_, args, context, info) => {
             const newAttribute = context.prisma.attribute.create({
                 data: {
@@ -80,11 +106,12 @@ module.exports = {
             console.log(newAttribute)
             return newAttribute
         },
+
         addAttributeToProduct: (_, args, context, info) => {
             const updateProd = context.prisma.amazonProduct.update({
                 where: {
                     asin: args.asin
-                }, 
+                },
                 data: {
                     attributes: {
                         create: {
@@ -99,20 +126,91 @@ module.exports = {
             })
             return updateProd
         },
+
         deleteAttributeFromProduct: (_, args, context, info) => {
             const updateProd = context.prisma.amazonProduct.update({
                 where: {
                     asin: args.asin
-                }, 
+                },
                 data: {
                     attributes: {
-                        deleteMany:{
+                        deleteMany: {
                             AttributeId: parseInt(args.attribute)
                         }
                     }
                 }
             })
             return updateProd
+        },
+
+        updateSize: (_, args, context, info) => {
+            const updateProd = context.prisma.amazonProduct.update({
+                where: {
+                    asin: args.asin
+                },
+                data: {
+                    size_length: args.size_length,
+                    size_height: args.size_height,
+                    size_width: args.size_width,
+                    size_units: args.size_units,
+                }
+            })
+            return updateProd
+        },
+
+        updateWeight: (_, args, context, info) => {
+            const updateProd = context.prisma.amazonProduct.update({
+                where: {
+                    asin: args.asin
+                },
+                data: {
+                    weight: args.weight,
+                    weight_units: args.weight_units,
+                }
+            })
+            return updateProd
+        },
+
+        deleteProduct: async (_, args, context, info) => {
+            const delRelation = context.prisma.amazonProductAttribute.deleteMany({
+                where: {
+                    AmazonProductId: parseInt((await context.prisma.amazonProduct.findMany({
+                        where: {
+                            asin: args.asin
+                        }
+                    }))[0].id)
+                }
+            })
+            const delProd = context.prisma.amazonProduct.delete({
+                where: {
+                    asin: args.asin
+                }
+            })
+
+            const transaction = context.prisma.$transaction([delRelation, delProd])
+
+            return transaction
+        },
+
+        deleteAttribute: async (_, args, context, info) => {
+            const delRelation = context.prisma.amazonProductAttribute.deleteMany({
+                where: {
+                    AttributeId: parseInt((await context.prisma.attribute.findMany({
+                        where: {
+                            id: parseInt(args.attribute)
+                        }
+                    }))[0].id)
+                }
+            })
+            const delAttr = context.prisma.attribute.delete({
+                where: {
+                    id: parseInt(args.attribute)
+                }
+            })
+
+            const transaction = context.prisma.$transaction([delRelation, delAttr])
+
+            return transaction
         }
     },
 
