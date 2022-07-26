9cc8e6fe1ea69243f0b5900301f9df5121c2885c
Suyash Sanjeev <ssuyash@uw.edu>
2022-07-26 11:41:14 -0700

    fixed undefined bug

--- src/components/AssignmentAlgorithm.ts
+++ src/components/AssignmentAlgorithm.ts
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-export const getRandomObjectsFromList = (n: any, objects: string[]) =>  {
+export const getRandomObjectsFromList = (n: number, objects: string[]) =>  {
     let output: any[] = [];
     for (let i = 0; i < n; i++) {
         output.push(_.sample(objects));
@@ -8,11 +8,21 @@ export const getRandomObjectsFromList = (n: any, objects: string[]) =>  {
     return output;
 }
 
-export const randomlyAssignObjects = (numOfBins: number, numObjects: number, objectList: string[]) => {
-    let numBins = numOfBins;
+export const randomlyAssignObjects = (numBins: number, numObjects: number, objectList: string[]) => {
+    //empty case
+    let contents: string[][] = [[]];
+    if (numObjects == undefined) {
+        while (numBins > 0) {
+            let empty = [""];
+            contents.push(empty);
+            numBins--; 
+        }
+    }
+
+    let fillWithEmpty = false;
     let numObjectsCopy = numObjects;
     let evenSplit: number[] = [];
-    let contents: string[][] = [[]];
+    //case when number of objects exceeds number of bins
     if (numObjectsCopy > numBins) {
         while (numObjectsCopy > 0) {
             const truncatedDivision = Math.floor(numObjectsCopy/numBins);
@@ -21,20 +31,50 @@ export const randomlyAssignObjects = (numOfBins: number, numObjects: number, obj
             numBins--;
         }
     }
+    //case when number of objects is less than number of bins
     else {
+        fillWithEmpty = true;
         while(numObjectsCopy > 0) {
             evenSplit.push(1);
             numObjectsCopy--;
+            numBins--;
         }
     }
     numObjectsCopy = numObjects;
+
 
     for (let i = 0; i < numObjectsCopy; i++) {
         let x = Math.floor(Math.random() * evenSplit.length);
         contents.push(getRandomObjectsFromList(evenSplit[x], objectList));
         evenSplit.splice(x, 1);
     }
+    //filling with empty boxes for case when number of objects is less than number of bins
+    if (fillWithEmpty) {
+        while (numBins > 0) {
+            let empty = [""];
+            contents.push(empty);
+            numBins--; 
+        }
+    }
     contents.splice(0,1);
 
-    return contents;
+    return shuffle(contents);
 }
+
+function shuffle(array: any[]) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
