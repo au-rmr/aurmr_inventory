aabc11aa8bd759a16a7265fa6baf802b2b8aedae
Suyash Sanjeev <ssuyash@uw.edu>
2022-08-17 12:51:28 -0700

    ros picks

--- src/GraphQLQueriesMuts/Mutation.tsx
+++ src/GraphQLQueriesMuts/Mutation.tsx
@@ -13,6 +13,14 @@ export const ADD_PROD_TO_BIN_FOR_AN_EVAL = gql`
 export const ADD_PICK_FOR_AN_EVAL = gql`
     mutation addPickWithOnlyProdBin($ProductBinId: Int!) {
         addPickWithOnlyProdBin(ProductBinId: $ProductBinId) {
+            id
+        }
+    }
+`
+
+export const ADD_OUTCOME_AND_TIME_FOR_A_PICK = gql`
+    mutation addOutcomeAndTimeToPick($id: String!, $Outcome: Boolean!, $TimeTakenSec: Float!) {
+        addOutcomeAndTimeToPick(id: $id, Outcome: $Outcome, TimeTakenSec: $TimeTakenSec) {
             ProductBinId
         }
     }
--- src/components/PickHandler.tsx
+++ src/components/PickHandler.tsx
@@ -3,9 +3,11 @@ import { useState } from 'react';
 import { useQuery, useMutation } from '@apollo/client';
 import { Button } from '@mui/material';
 import { GET_PROD_BIN_IDS, GET_PICKS_FROM_PROD_BIN_IDS, GET_PROD_FROM_EVAL } from '../GraphQLQueriesMuts/Query';
-import { ADD_PICK_FOR_AN_EVAL } from '../GraphQLQueriesMuts/Mutation';
+import { ADD_PICK_FOR_AN_EVAL, ADD_OUTCOME_AND_TIME_FOR_A_PICK } from '../GraphQLQueriesMuts/Mutation';
 import { useCSVReader } from 'react-papaparse';
 import ObjectTable from './ObjectTable';
+
+import ROSLIB from "roslib";
 
 interface PickHandlerProps {
 }
@@ -18,16 +20,50 @@ interface TableObject {
 }
 
 function PickHandler(props: PickHandlerProps) {
+    const debug: boolean = true;
     const [evalTextArea, setEvalTextArea] = useState<string>("");
     const [pickTextArea, setPickTextArea] = useState<string>("");
     const [uploadedData, setUploadedData] = useState<string[]>();
+    const [pickTimes, setPickTimes] = useState<number[]>([]);
     const [picks, setPicks] = useState<TableObject[]>([]);
+    const [robotIsMoving, setRobotIsMoving] = useState<boolean>(false);
     const [errorObjects, setErrorObjects] = useState<string[]>([]);
     const { CSVReader } = useCSVReader();
     const { refetch: prodBinRefetch } = useQuery(GET_PROD_BIN_IDS);
     const { refetch: picksFromProdBinRefetch } = useQuery(GET_PICKS_FROM_PROD_BIN_IDS);
     const { refetch: prodFromEvalRefetch } = useQuery(GET_PROD_FROM_EVAL);
     const [add_pick] = useMutation(ADD_PICK_FOR_AN_EVAL);
+    const [add_info_to_pick] = useMutation(ADD_OUTCOME_AND_TIME_FOR_A_PICK);
+
+    const [robotServiceClient, setRobotServiceClient] = useState<any>(0);
+    const [isConnected, setIsConnected] = useState<boolean>(false);
+    function connectToRos() {
+        let ros: any;
+        ros = new ROSLIB.Ros({
+            url: 'ws://control:9090'
+        });
+
+        ros.on('connection', function () {
+            console.log('Connected to websocket server.');
+            setIsConnected(true);
+        });
+
+        ros.on('error', function (error: any) {
+            console.log('Error connecting to websocket server: ', error);
+            setIsConnected(false);
+        });
+
+        ros.on('close', function () {
+            console.log('Connection to websocket server closed.');
+            setIsConnected(false);
+        });
+
+        setRobotServiceClient(new ROSLIB.Service({
+            ros: ros,
+            name: "aurmr_demo/pick",
+            serviceType: "/aurmr_tasks/PickRequest"
+        }));
+    }
 
     function parsePickText() {
         let num = parseInt(pickTextArea as string);
@@ -47,10 +83,11 @@ function PickHandler(props: PickHandlerProps) {
         if (uploadedData && evalTextArea && numPicks) {
             for (let i = 0; i < uploadedData.length; i++) {
                 const asinValue = uploadedData[i][0];
-                let objectsInEval = await prodFromEvalRefetch({evalName: evalTextArea, asin: asinValue});
+                let objectsInEval = await prodFromEvalRefetch({ evalName: evalTextArea, asin: asinValue });
                 // object not in eval check
-                if (objectsInEval.data.getAmazonProductFromEval.length > 0) {
-                    let value = await prodBinRefetch({asin: asinValue, binId: objectsInEval.data.getAmazonProductFromEval[0].bins[0].bin.BinId, evalName: evalTextArea});
+                if (objectsInEval.data.getAmazonProductFromEval.length > 0 && objectsInEval.data.getAmazonProductFromEval[0].bins.length > 0) {
+                    console.log(objectsInEval);
+                    let value = await prodBinRefetch({ asin: asinValue, binId: objectsInEval.data.getAmazonProductFromEval[0].bins[0].bin.BinId, evalName: evalTextArea });
                     objects.push({
                         asin: asinValue,
                         productName: objectsInEval.data.getAmazonProductFromEval[0].name,
@@ -71,17 +108,42 @@ function PickHandler(props: PickHandlerProps) {
                 let picksAssociatedWithProdBinId;
                 for (let j = 0; j < currentObject.productBinId.length; j++) {
                     id = parseInt(currentObject.productBinId[j].id);
-                    picksAssociatedWithProdBinId = await picksFromProdBinRefetch({ProductBinId: id});
+                    picksAssociatedWithProdBinId = await picksFromProdBinRefetch({ ProductBinId: id });
                     if (picksAssociatedWithProdBinId.data.getPicksFromProductBin.length === 0) {
                         break;
                     } else {
                         id = -1;
                     }
                 }
-                
+
                 if (id > -1) {
                     currentObject.productBinId = id;
-                    await add_pick({variables: {ProductBinId: id}});
+                    const pickResult = await add_pick({ variables: { ProductBinId: id } });
+                    const pickId = pickResult.data.addPickWithOnlyProdBin.id;
+                    // Record current time
+                    const startTime = Date.now();
+                    // We send the message to ros with info; wait for ros to return a boolean
+                    var request = new ROSLIB.ServiceRequest({
+                        bin_id: currentObject.binName,
+                        object_id: currentObject.productBinId
+                    });
+                    console.log(robotServiceClient);
+                    setRobotIsMoving(true);
+                    if (!debug) {
+                        robotServiceClient.callService(request, async function (result: any) {
+                            console.log("Received back from the Robot: " +
+                                robotServiceClient.name +
+                                ': ' +
+                                result);
+                            setRobotIsMoving(false);
+                            // After the return value, record current time and subtract start time
+                            const timeElapsed = (Date.now() - startTime) / 1000;
+                            setPickTimes(current => [...current, timeElapsed]);
+                            console.log(`seconds elapsed = ${timeElapsed}`);
+                            // Mutate the pick and add the outcome and time taken
+                            await add_info_to_pick({ variables: { id: pickId, Outcome: result, TimeTakenSec: timeElapsed } })
+                        });
+                    }
                     setPicks(current => [...current, currentObject]);
                 } else {
                     setErrorObjects(current => [...current, currentObject.asin]);
@@ -91,82 +153,106 @@ function PickHandler(props: PickHandlerProps) {
         }
     }
 
+
+    function renderWaitPage() {
+        <div>
+            <div id="heading-text">Robot in motion. Please wait...</div>
+            {debug ? <Button onClick={() => setRobotIsMoving(false)}>Received Message</Button> : <br />}
+        </div>
+    }
+
     return (
-        <>
-            <div id = "left-content">
-                <div id="heading-text">Upload File</div>
-                <CSVReader
-                    onUploadAccepted={(results: any) => {
-                        setUploadedData(results.data)
-                    }}
-                >
-                    {({
-                        getRootProps,
-                        acceptedFile,
-                        ProgressBar,
-                        getRemoveFileProps
-                    }: any) => (
-                        <>
-                            <div>
-                                <Button id="pick-button" variant="outlined" onClick={onSubmit} {...getRootProps()}>
-                                    Browse
-                                </Button>
-                                <div id="text">
-                                    {acceptedFile && acceptedFile.name}
-                                </div>
-                                <Button id="pick-button" variant="outlined" color="error" {...getRemoveFileProps()}>
-                                    Remove
-                                </Button>
-                            </div>
-                            <ProgressBar/>
-                        </>
-                    )}
-                </CSVReader>
-                <br/>
-                <div id="heading-text">Number of Picks</div>
-                <textarea
-                    id="pick-text-area"
-                    onChange={(event) => setPickTextArea(event.target.value)}
-                    onKeyPress={e => {
-                        if (e.key === 'Enter') {
-                            e.preventDefault();
-                        }
-                    }}
-                    value={pickTextArea}
-                /> <br/>
-                <div id="heading-text">Evalution Name</div>
-                <textarea
-                    id="pick-text-area"
-                    onChange={(event) => setEvalTextArea(event.target.value)}
-                    onKeyPress={e => {
-                        if (e.key === 'Enter') {
-                            e.preventDefault();
-                            onSubmit();
-                        }
-                    }}
-                    value={evalTextArea}
-                /> <br/>
+        <div>
+            {robotIsMoving ? <div>
+                <div id="heading-text">Robot in motion. Please wait...</div>
+                {debug ? <Button onClick={() => setRobotIsMoving(false)}>Received Message</Button> : <br />}
+            </div> :
+                <>
+                    <div id="left-content">
+                        <div id="heading-text">Upload File</div>
+                        <CSVReader
+                            onUploadAccepted={(results: any) => {
+                                setUploadedData(results.data)
+                            }}
+                        >
+                            {({
+                                getRootProps,
+                                acceptedFile,
+                                ProgressBar,
+                                getRemoveFileProps
+                            }: any) => (
+                                <>
+                                    <div>
+                                        <Button id="pick-button" variant="outlined" onClick={onSubmit} {...getRootProps()}>
+                                            Browse
+                                        </Button>
+                                        <div id="text">
+                                            {acceptedFile && acceptedFile.name}
+                                        </div>
+                                        <Button id="pick-button" variant="outlined" color="error" {...getRemoveFileProps()}>
+                                            Remove
+                                        </Button>
+                                    </div>
+                                    <ProgressBar />
+                                </>
+                            )}
+                        </CSVReader>
+                        <br />
+                        <div id="heading-text">Number of Picks</div>
+                        <textarea
+                            id="pick-text-area"
+                            onChange={(event) => setPickTextArea(event.target.value)}
+                            onKeyPress={e => {
+                                if (e.key === 'Enter') {
+                                    e.preventDefault();
+                                }
+                            }}
+                            value={pickTextArea}
+                        /> <br />
+                        <div id="heading-text">Evalution Name</div>
+                        <textarea
+                            id="pick-text-area"
+                            onChange={(event) => setEvalTextArea(event.target.value)}
+                            onKeyPress={e => {
+                                if (e.key === 'Enter') {
+                                    e.preventDefault();
+                                    onSubmit();
+                                }
+                            }}
+                            value={evalTextArea}
+                        /> <br />
 
-                <Button id="pick-button" variant="contained" color="success" onClick={onSubmit}>Submit</Button>
-                
-            </div>
-            
-            <div id="left-content">
-                <div id="heading-text">Pick Info</div>
-            </div>
-            <ObjectTable objectList={picks}/>
+                        <Button id="pick-button" variant="contained" color="success" onClick={onSubmit}>Submit</Button>
 
-            <div id="left-content">
-                <div id="heading-text">Error ASINs</div>
-                {
-                    errorObjects && errorObjects.map((value, index) => {
-                        return (<div key={index} id="text">{value}</div>)
-                    })
-                }
-            </div>
-        </>
+                    </div>
+
+                    <div id="left-content">
+                        <div id="heading-text">Pick Info</div>
+                    </div>
+                    <ObjectTable objectList={picks} />
+
+                    <div id="left-content">
+                        <div id="heading-text">Error ASINs</div>
+                        {
+                            errorObjects && errorObjects.map((value, index) => {
+                                return (<div key={index} id="text">{value}</div>)
+                            })
+                        }
+                    </div>
+
+                    <div>
+                        {
+                            pickTimes && pickTimes.map((value, index) => {
+                                return (<div key={index} id="text">{index + 1}. {value} seconds</div>)
+                            })
+                        }
+                    </div>
+                </>
+            }
+
+        </div>
     );
-    
+
 }
 
 export default PickHandler;
