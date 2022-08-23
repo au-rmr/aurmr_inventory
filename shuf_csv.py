ca92fa080ab858efd0ac3501036deca27cf1059f
Markus Grotz <grotz@uw.edu>
2022-08-22 21:24:34 -0700

    update shuffle script

--- shuf_csv.py
+++ shuf_csv.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
 import rich_click as click
-import pandas as pd
-import numpy as np
 from click_prompt import FilePathOption
 from rich import print
 
+import pandas as pd
 
 
 @click.command()
@@ -14,19 +13,16 @@ from rich import print
 def parse_csv(csv_file, output):
     print(f'Reading {csv_file}')
     print(f'Output will be {output}')
-    bins, objects = [], []
-    names = ['object', 'bin']
-    df = pd.read_csv(csv_file, names = names, dtype=dict(zip(names, ['str', 'str'])))
-    df = df.sample(frac=1)
+    names = ['object', 'bin', 'description']
+    df = pd.read_csv(csv_file, names = names, dtype=dict(zip(names, ['str', 'str', 'str'])))
     print(df.describe())
 
-    for name, g_df in df.groupby('bin'):
-        o, b = g_df.values[0]
-        objects.append(o.strip())
-        bins.append(b.strip())
+    df = df.groupby('bin').sample(n=1)
+    # df = df.groupby('bin').sample(frac=1)
 
-    print(list(zip(bins, objects)))
-    pd.DataFrame({'object': objects, 'bin': bins}).to_csv(output, header=False, index=False)
+    df.to_csv(output, header=False, index=False)
+
+
 
 
 if __name__ == '__main__':
